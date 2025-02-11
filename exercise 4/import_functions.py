704fc5a5da0ab5c5dd8d32947a8176cbce0b56ac
mikaylahubbard <168701970+mikaylahubbard@users.noreply.github.com>
2025-02-10 19:23:00 -0500

    Create import_functions.py

---
+++ exercise 4/import_functions.py
@@ -0,0 +1,5 @@
+def describe_city(name, country="the united states"):
+    print(f"{name.title()} is in {country.title()}")
+    
+def city_country(name, country):
+    return f"{name.title()}, {country.title()}"
