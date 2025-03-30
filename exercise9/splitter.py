6d045b9a769d41b0667dfff401158ef3760c1d6b
mikaylahubbard <168701970+mikaylahubbard@users.noreply.github.com>
2025-03-30 18:52:09 -0400

    Update splitter.py

--- exercise9/splitter.py
+++ exercise9/splitter.py
@@ -1,1 +1,12 @@
+plaintext = input("What phrase would you like to encrypt?: ")
+length = len(plaintext)
 
+message = f"""
+Qfter how many characters would you like the split to happen> Enter a number between 1 and {length-1}: 
+"""
+splitter = int(input(message))
+
+ciphertext = ""
+ciphertext = plaintext[splitter:] + plaintext[:splitter]
+
+print(ciphertext)
